ce20324784f697e702dc50c94f108afcc2daa274
Sunny-62 <sunnykumar327987@gmail.com>
2023-12-01 16:10:13 +0530

    second change

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ the.innerText;
 head.appendChild(the);
 
 var columns=26;
+var currcell;
 for(var column=0;column<columns;column++){
     var the=document.createElement("th")
      the.innerText=String.fromCharCode(column+65);
@@ -34,13 +35,67 @@ body.appendChild(tr);
 }
 function onfocusfun(event){
     console.log("in focus:",event.target.id);
+    currcell=event.target;
     document.querySelector('#current-cell').innerText=event.target.id;
 }
+var boldbtn=document.querySelector('#bold-btn')
+var italicbtn=document.querySelector('#italic-btn')
+var underlinebtn=document.querySelector('#underline-btn')
+var textcolor=document.querySelector('#text-color')
+var bgcolor=document.querySelector('#bg-color')
+var leftalign=document.querySelector('#left-align')
+var rightalign=document.querySelector('#right-align')
+var centeralign=document.querySelector('#center-align')
+
+boldbtn.addEventListener("click",()=>{
+    console.log(currcell);
+    if(currcell.style.fontWeight=="bold"){
+        currcell.style.fontWeight="normal"; 
+    }
+    else{
+        currcell.style.fontWeight="bold";
+    }
+})
+italicbtn.addEventListener("click",()=>{
+    console.log(currcell);
+    if(currcell.style.fontStyle=="italic"){
+        currcell.style.fontStyle="normal"; 
+    }
+    else{
+        currcell.style.fontStyle="italic";
+    }
+})
+underlinebtn.addEventListener("click",()=>{
+    console.log(currcell);
+    if(currcell.style.textDecoration=="underline"){
+        currcell.style.textDecoration=null; 
+    }
+    else{
+        currcell.style.textDecoration="underline";
+    }
+})
+textcolor.addEventListener("change",()=>{
+currcell.style.color=textcolor.value;
+})
+bgcolor.addEventListener("change",()=>{
+    currcell.style.backgroundColor=bgcolor.value;
+    })
+    leftalign.addEventListener("click",()=>{
+
+currcell.style.textAlign="left";
+    })
+    rightalign.addEventListener("click",()=>{
+
+        currcell.style.textAlign="right";
+            })
+            centeralign.addEventListener("click",()=>{
+
+                currcell.style.textAlign="center";
+                    })
+                    
+                
+        
 
 
 
 
-
-
-
-//})
